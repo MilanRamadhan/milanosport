9e40b8dbadb435ac5a6d9c3cac09710085c5d2ce
milan <milan23@mhs.usk.ac.id>
2025-12-17 15:17:55 +0700

    feat: update adminapi

--- src/api/adminApi.ts
+++ src/api/adminApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { InternalAxiosRequestConfig } from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://192.168.1.60:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://milano-sport-backend.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
